4b85d0d5a8c404c66ea52c93b2105167faa30413
bnestes18 <bnestes2014@gmail.com>
2023-04-23 19:37:29 -0400

    Day 8 Challenge

---
+++ day-8/main.py
@@ -0,0 +1,38 @@
+# This function determines the maximum even digit and minimum odd digit and subtracts the two.
+def odd_even(list_of_nums):
+    odds_list = []
+    evens_list = []
+    for num in list_of_nums:
+        if num % 2 == 0:
+            evens_list.append(num)
+        else:
+            odds_list.append(num)
+            
+    evens_list.sort()
+    odds_list.sort(reverse=True)
+    
+    max_even = evens_list[-1]
+    min_odd = odds_list[-1]
+    difference = max_even - min_odd
+
+    text = "{} - {} = {}"
+    
+    return text.format(max_even, min_odd, difference)
+
+print("odd_even function", odd_even([1,2,4,6]))
+
+
+# List of Prime Numbers
+# Could not figure this one out on my own!!!! Grrrr!!!
+def prime_numbers(integer):
+    primes = []
+    for i in range(0, integer+1):
+        if i > 1:
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                primes.append(i)
+    return primes
+            
+print("prime_numbers function:", prime_numbers(10))
